bf5bf87c2a20fd4e25c696542888078d9200371e
Khoa Huynh <khoawin92@gmail.com>
2018-08-11 13:33:17 -0700

    added persistance

--- src/reducer/category.js
+++ src/reducer/category.js
@@ -1,8 +1,8 @@
-let initialState = [];
+let initialState = localStorage && localStorage.state && JSON.parse(localStorage.state) || [];
 
 export default (state = initialState, action) => {
 
-
+  let newState;
   let { type, payload } = action;
 
   console.log('in reducer');
@@ -11,18 +11,28 @@ export default (state = initialState, action) => {
 
   switch (type) {
 
-    case 'CATEGORY_CREATE': return [...state, payload];
+    case 'CATEGORY_CREATE':
+      newState = [...state, payload];
+      localStorage.state = JSON.stringify(newState);
+      return newState;
 
-    case 'CATEGORY_UPDATE': return state.map(category => {
-      if (category.id === payload.id) {
-        payload.editing = false;
-        return payload;
-      } else {
-        return category;
-      }
-    });
+    case 'CATEGORY_UPDATE':
+      newState = state.map(category => {
+        if (category.id === payload.id) {
+          payload.editing = false;
+          return payload;
+        } else {
+          return category;
+        }
+      });
+      localStorage.state = JSON.stringify(newState);
+      return newState;
 
-    case 'CATEGORY_DESTROY': return state.filter(category => category.id !== payload.id);
+
+    case 'CATEGORY_DESTROY':
+      newState = state.filter(category => category.id !== payload.id);
+      localStorage.state = JSON.stringify(newState);
+      return newState;
 
     case 'CATEGORY_EDIT': return state.map(category => {
       if (category.id === payload.id) {
